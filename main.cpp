116cf3c75638d908e0c2da0da361cc0ef2b538fb
sagge14 <108992042+sagge14@users.noreply.github.com>
2022-08-24 15:21:14 +0300

    delete if(true)

--- main.cpp
+++ main.cpp
@@ -1,24 +1,17 @@
-#include <memory>
 #include "dog.h"
 #include "shared_ptr_toy.h"
 
 int main() {
     shared_ptr_toy a("Ball");
     shared_ptr_toy b("Bone");
-    auto* c = new shared_ptr_toy("Toy*"); //в такой случае придется удалять в ручную..
 
-    if(true)
-        Dog EE(*c);
-
-    delete c; // даляем Toy*
-
-    if(true)
     {
         Dog ABC(make_shared_ptr_toy("myToy3"));
 
-        if(true)
+        {
             Dog E(b);
-        
+        }
+
         Dog AB(make_shared_ptr_toy(b));
         ABC.setLovelyToy(a);
     }
