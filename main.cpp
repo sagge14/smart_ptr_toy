2411c683b0e60c1006039829bbe003c9ee9943b0
sagge14 <108992042+sagge14@users.noreply.github.com>
2022-08-25 16:45:32 +0300

    delete int*, ~dog

--- main.cpp
+++ main.cpp
@@ -2,19 +2,16 @@
 #include "shared_ptr_toy.h"
 
 int main() {
-    shared_ptr_toy a("Ball");
-    shared_ptr_toy b("Bone");
-
     {
+        shared_ptr_toy a("Ball");
+        shared_ptr_toy b("Bone");
         Dog ABC(make_shared_ptr_toy("myToy3"));
-
         {
             Dog E(b);
         }
-
         Dog AB(make_shared_ptr_toy(b));
         ABC.setLovelyToy(a);
     }
-
+    system("pause");
 return 0;
 }
